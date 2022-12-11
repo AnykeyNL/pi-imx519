81c40951c87475e1edf9b977f6b21d5bdeadc40f
Richard Garsthagen <the.anykey@gmail.com>
2022-12-11 19:46:58 +0100

    Create setip.py

---
+++ setip.py
@@ -0,0 +1,19 @@
+fid = open('/boot/id.txt','r')
+fi = open('/etc/dhcpcd.conf','w')
+
+id = fid.read()
+
+print ("Setting IP address with ID: {}".format(id))
+
+id = id.strip()
+
+fi.write("#Do NOT edit, file is controlled by setid python script\n")
+fi.write("\n")
+fi.write("interface eth0\n")
+
+fi.write("static ip_address=192.168.1." + id + "/24\n")
+fi.write("static routers=192.168.1.254\n")
+fi.write("static domain_name_servers=192.168.1.254\n")
+
+fid.close()
+fi.close()
