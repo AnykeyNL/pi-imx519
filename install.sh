56fdbda2bf8bd2436c7ba5311ec02a4d12263d6e
Richard Garsthagen <the.anykey@gmail.com>
2022-12-11 19:59:37 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,4 +13,4 @@ echo 99 | sudo tee -a /boot/id.txt
 sudo wget -o /etc/init.d/setidtoip https://raw.githubusercontent.com/AnykeyNL/pi-imx519/main/setidtoip 
 sudo wget -o /etc/init.d/setip.py https://raw.githubusercontent.com/AnykeyNL/pi-imx519/main/setip.py
 sudo chmod 755 /etc/init.d/setidtoip
-
+sudo update-rc.d setidtoip defaults
