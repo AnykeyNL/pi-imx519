121d9ba57adb26537c1925af8090651911b68f28
Richard Garsthagen <the.anykey@gmail.com>
2022-12-11 18:54:35 +0100

    Update and rename install.md to install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+sudo raspi-config
+- enable ssh
+
 wget -O install_pivariety_pkgs.sh https://github.com/ArduCAM/Arducam-Pivariety-V4L2-Driver/releases/download/install_script/install_pivariety_pkgs.sh 
 chmod +x install_pivariety_pkgs.sh
 ./install_pivariety_pkgs.sh -p libcamera_dev
