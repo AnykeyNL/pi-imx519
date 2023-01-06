98c57cb153f0c1f93d484e97b9256fbb4852852c
Richard Garsthagen <the.anykey@gmail.com>
2023-01-06 16:17:24 +0100

    Update setip.py

--- setip.py
+++ setip.py
@@ -11,9 +11,9 @@ fi.write("#Do NOT edit, file is controlled by setid python script\n")
 fi.write("\n")
 fi.write("interface eth0\n")
 
-fi.write("static ip_address=192.168.1." + id + "/24\n")
-fi.write("static routers=192.168.1.254\n")
-fi.write("static domain_name_servers=192.168.1.254\n")
+fi.write("static ip_address=192.168.99." + id + "/24\n")
+fi.write("static routers=192.168.99.1\n")
+fi.write("static domain_name_servers=192.168.99.1\n")
 
 fid.close()
 fi.close()
